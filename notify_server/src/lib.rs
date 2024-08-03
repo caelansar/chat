8cd159d9842f3d46237b3efbb53d06dd07dc2766
caelansar <819711623@qq.com>
2024-08-03 21:30:49 +0800

    feat: introduce AppState/AppError

--- notify_server/src/lib.rs
+++ notify_server/src/lib.rs
@@ -1,42 +1,65 @@
+mod config;
+mod error;
 mod sse;
 
+pub use crate::config::AppConfig;
+use crate::error::AppError;
 use axum::{
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
-use chat_core::{Chat, Message};
+use chat_core::middlewares::TokenVerify;
+use chat_core::{Chat, DecodingKey, Message, User};
+use dashmap::DashMap;
 use futures::StreamExt;
 use sqlx::postgres::PgListener;
 use sse::sse_handler;
+use std::ops::Deref;
+use std::sync::Arc;
+use tokio::sync::broadcast;
 use tracing::info;
 
 const INDEX_HTML: &str = include_str!("../index.html");
 
-pub enum Event {
+pub type UserMap = Arc<DashMap<u64, broadcast::Sender<Arc<AppEvent>>>>;
+
+#[derive(Clone)]
+pub struct AppState(Arc<AppStateInner>);
+
+pub struct AppStateInner {
+    pub config: AppConfig,
+    #[allow(unused)]
+    users: UserMap,
+    dk: DecodingKey,
+}
+
+pub enum AppEvent {
     NewChat(Chat),
     AddToChat(Chat),
     RemoveFromChat(Chat),
     NewMessage(Message),
 }
 
-pub fn get_router() -> Router {
+pub fn get_router(config: AppConfig) -> Router {
+    let state = AppState::new(config);
+
     Router::new()
         .route("/", get(index_handler))
         .route("/events", get(sse_handler))
+        .with_state(state.clone())
 }
 
-pub async fn setup_pg_listener() -> anyhow::Result<()> {
-    let mut listener =
-        PgListener::connect("postgresql://postgres:postgres@localhost:5432/chat").await?;
+pub async fn setup_pg_listener(config: AppConfig) -> anyhow::Result<()> {
+    let mut listener = PgListener::connect(&config.server.db_url).await?;
     listener.listen("chat_updated").await?;
     listener.listen("chat_message_created").await?;
 
     let mut stream = listener.into_stream();
 
     tokio::spawn(async move {
-        while let Some(Ok(notif)) = stream.next().await {
-            info!("Received notification: {:?}", notif);
+        while let Some(Ok(notification)) = stream.next().await {
+            info!("Received notification: {:?}", notification);
         }
     });
 
@@ -45,4 +68,28 @@ pub async fn setup_pg_listener() -> anyhow::Result<()> {
 
 async fn index_handler() -> impl IntoResponse {
     Html(INDEX_HTML)
+}
+
+impl TokenVerify for AppState {
+    type Error = AppError;
+
+    fn verify(&self, token: &str) -> Result<User, Self::Error> {
+        Ok(self.dk.verify(token)?)
+    }
+}
+
+impl Deref for AppState {
+    type Target = AppStateInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl AppState {
+    pub fn new(config: AppConfig) -> Self {
+        let dk = DecodingKey::load(&config.auth.pk).expect("Failed to load public key");
+        let users = Arc::new(DashMap::new());
+        Self(Arc::new(AppStateInner { config, dk, users }))
+    }
 }
--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use notify_server::{get_router, setup_pg_listener};
+use notify_server::{get_router, setup_pg_listener, AppConfig};
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
@@ -9,11 +9,14 @@ async fn main() -> Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let addr = "0.0.0.0:6687";
+    let config = AppConfig::load()?;
 
-    setup_pg_listener().await?;
+    let app = get_router(config.clone());
 
-    let app = get_router();
+    let addr = format!("0.0.0.0:{}", config.server.port);
+
+    setup_pg_listener(config).await?;
+
     let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
 
